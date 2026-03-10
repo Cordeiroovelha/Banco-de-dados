bed8e78a5484e119562109419e12d11e9dee08f9
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-09 22:29:47 -0300

    Aula 09/03/26

--- ConsultaFuncionarios.sql
+++ ConsultaFuncionarios.sql
@@ -43,3 +43,125 @@ GO
 -- exibe os dados da tabela FUNCIONARISO --
 SELECT * FROM FUNCIONARIOS;
 GO
+
+-------------------------------------------
+
+-- SELECT --
+
+SELECT Nome 'Name'
+FROM FUNCIONARIOS;
+GO
+
+SELECT Nome[Name]
+FROM FUNCIONARIOS;
+GO
+
+SELECT ID      AS 'Identificador',
+       Nome,
+       Sexo,
+       Salario AS 'Salário'
+FROM FUNCIONARIOS;
+GO
+
+SELECT * 
+FROM FUNCIONARIOS;
+GO
+
+SELECT TOP 1 * 
+FROM FUNCIONARIOS;
+GO
+
+SELECT TOP 1
+    Id,
+    Nome
+FROM FUNCIONARIOS;
+GO
+
+-- Comandos de consulta --
+
+-- WHERE--
+
+SELECT * FROM FUNCIONARIOS
+WHERE Sexo = 'm' AND
+      Salario > 1000;
+GO
+
+-- ORDER BY crescente e decrescente --
+
+SELECT * FROM FUNCIONARIOS
+ORDER BY Nome;
+GO
+
+SELECT * FROM FUNCIONARIOS
+ORDER BY Nome DESC;
+GO
+
+-- inserir valor com nome igual a um anterior --
+INSERT INTO FUNCIONARIOS VALUES (6, 'Duch Van Der Linde', 'm', '05/03/2026', 5000.00);
+GO
+
+SELECT * FROM FUNCIONARIOS
+WHERE Salario > 1000
+ORDER BY Nome,
+         Salario DESC;
+GO
+
+-- Alterar a estrutura da tabela --
+
+-- tabela teste --
+
+CREATE TABLE TESTE(
+    ID INT,
+    Nome CHAR(10)
+);
+GO
+
+-- alterar uma coluna --
+ALTER TABLE TESTE
+    ALTER COLUMN Nome CHAR(50);
+Go
+
+-- Add uma coluna --
+ALTER TABLE TESTE
+    ADD DataNascimento DATE,
+        Peso DECIMAL(5,2);
+Go
+
+-- Deleta uma coluna --
+ALTER TABLE TESTE
+    DROP COLUMN DataNascimento;
+GO
+
+-- Adiciona Rstrição --
+ALTER TABLE TESTE
+    ADD UNIQUE (Sexo);
+Go
+
+-- Altera restriçoes da coluno --
+ALTER TABLE TESTE
+ ALTER COLUMN ID INT NOT NULL;
+GO
+
+ALTER TABLE TESTE
+ ADD CONSTRAINT pk_id PRIMARY KEY (ID)
+GO
+
+-- Verificar todos os dados da tabela --
+EXEC sp_columns TESTE;
+Go
+
+-- Verificar nome da restriçao --
+EXEC sp_helpconstraint TESTE;
+Go
+
+-- Verificar alguns dados da tabela --
+Select TABLE_CATALOG    AS 'Banco de Dados',
+       TABLE_NAME       AS 'Tabela',
+       ORDINAL_POSITION AS 'Posiçao',
+       COLUMN_NAME       AS 'Coluna',
+       DATA_TYPE        AS 'Tipos de Dados',
+       COLLATION_NAME    AS 'Idioma da Coluna',
+       IS_NULLABLE      AS 'Aceita Nulo?'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'TESTE';
+GO

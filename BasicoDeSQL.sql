9776f9da024eabfa8e3e815428c2f755b6775d6f
Murilo Jutte Cordeiro <cordeiro.juttel@gmail.com>
2026-04-25 00:23:00 -0300

    organização de arquivos

--- BasicoDeSQL.sql
+++ BasicoDeSQL.sql
@@ -397,13 +397,407 @@ GO
 SELECT * FROM FuncionariosCOPIA
 GO
 
--- Tabela Clientes --
-CREATE TABLE CLIENTES (
-	ID    INT		   PRIMARY KEY,
-	Nome  VARCHAR(50)  NOT NULL,
-	Sexo  CHAR(1)	   NULL,
-	Idade INT		   NOT NULL CHECK (Idade > 18),
-	CPF   CHAR(11)	   NOT NULL UNIQUE,
-	Email VARCHAR(200) NOT NULL DEFAULT 'meu@email.com'
-);
+-- CAST e CONVERT --
+DECLARE @valor AS DECIMAL(5,2) = 156.90;
+
+SELECT CAST(@valor AS CHAR(20))       AS 'CAST',
+       CONVERT(DECIMAL(10,5), @valor) AS 'CONVERT'
+GO
+
+-- EXEMPLO 02 --
+DECLARE @valor AS DECIMAL(5,2) = 156.90;
+
+SELECT CAST(@valor AS CHAR(20))       AS 'CAST 1',
+       CAST(@valor AS DECIMAL(10,5))  AS 'CAST 2',
+       CONVERT(CHAR(20), @valor)      AS 'CONVERT 2',
+       CONVERT(DECIMAL(10,5), @valor) AS 'CONVERT'
+GO
+
+-- EXEMPLO 03 --
+DECLARE @data AS DATE = '25/01/2024';
+
+SELECT CAST(@data AS CHAR(20))   AS 'CAST (PADRÃO)',
+       CONVERT(CHAR(20), @data, 101)  AS 'CONVERT (usa) mm/dd/yyyy',
+       CONVERT(CHAR(20), @data, 140)  AS 'CONVERT (pt-br) dd/mm/aaaa',
+       CONVERT(CHAR(20), @data)  AS 'CONVERT (japão) yyyy/mm/dd'
+GO
+
+-- TRY_CONVERT TRY_PARSE --
+DECLARE @data AS CHAR(10) = '25/01/2024';
+
+----------------
+-- Incremento --
+----------------
+-- Cria uma sequência que começa em 1, com incremento 1 --
+CREATE SEQUENCE Incrementa1 AS INT
+    START WITH 1
+    INCREMENT BY 1;
+GO
+
+-- Cria uma sequência que começa em 10, com incremento 100 --
+CREATE SEQUENCE Incrementa100 AS INT
+    START WITH 10
+    INCREMENT BY 100;
+GO
+
+-- Cria uma sequência que começa em 1000, com incremento -100 --
+CREATE SEQUENCE Incrementa1000 AS INT
+    START WITH 1000
+    INCREMENT BY -100;
+GO
+
+-- Criar uma sequencia com mais parametros --
+CREATE SEQUENCE IncrementaDecimal AS DECIMAL(3,0)
+    START WITH 125
+    INCREMENT BY 25
+    MINVALUE 100
+    MAXVALUE 200
+    CYCLE
+    CACHE 3;
+GO
+
+----------------------------------------
+-- Exibe as sequencias e seus valores --
+----------------------------------------
+SELECT * FROM sys.sequences;
+GO
+
+-- Melhor --
+SELECT name          AS 'Nome',
+       create_date   AS 'Data de Criação',
+       start_value   AS 'Valor Inicial',
+       increment     AS 'Incremento',
+       minimum_value AS 'Valor Minimo',
+       maximum_value AS 'Valor Maximo',
+       current_value AS 'Valor Atual'
+FROM sys.sequences;
+GO
+
+-- Exive o primeiro valor das sequencias --
+SELECT NEXT VALUE FOR Incrementa1          AS 'Incrementa1',
+       NEXT VALUE FOR Incrementa100        AS 'Incrementa100',
+       NEXT VALUE FOR Incrementa1000       AS 'Incrementa1000',
+       NEXT VALUE FOR IncrementaDecimar    AS 'IncrementaDecimal';
+GO
+
+-- Recupera o valor atual de uma sequencia --
+SELECT current_value AS 'Valor Atual'
+FROM sys.sequences
+WHERE name = 'Incrementa100';
+GO
+
+-- Atribuição  de colunas --
+DECLARE @row AS INT;
+
+SET @row = (SELECT COUNT(*) FROM VIAGENS);
+
+SELECT @row AS 'Total de Viagens';
+GO
+
+-- IF ELSE --
+DECLARE @A AS INT = 10,
+        @B AS INT = 100,
+        @maior AS INT;
+
+IF @A > @B
+    SET @maior = @A;
+ELSE
+    SET @maior = @B;
+
+PRINT 'O maior valor: ' + CAST(@maior AS VARCHAR);
+GO
+
+-- Par e Impar --
+DECLARE @numero AS INT = 240;
+
+IF ((@numero % 2) = 0)
+    PRINT 'O numero ' + CAST(@numero AS VARCHAR) + ' é par!';
+ELSE
+    PRINT 'O numero ' + CAST(@numero AS VARCHAR) + ' é impar!';
+GO
+
+-- CASE --
+SELECT CodAluno AS 'Codigo',
+       NomeAluno AS 'Nome do Aluno',
+       Endereco  AS 'Endereço',
+       Genero    AS 'Genero',
+       CASE Genero
+            WHEN 'M' THEN 'Homem'
+            WHEN 'F' THEN 'Mulher'
+            ELSE 'Não declarado'
+       END AS 'Homem | Mulher'
+FROM ALUNOS;
+GO
+
+-- WHILE --
+DECLARE @i AS INT;
+SET @i = 1;
+
+WHILE @i <= 10
+    BEGIN
+        PRINT CAST(@i AS CHAR);
+        SET @i = @i + 1;
+    END;
+GO
+
+-- Tabuada usando WHILE --
+DECLARE @quantidade AS INT = 5,
+        @total AS INT = 1,
+        @contador AS INT,
+        @limite AS INT = 5;
+
+WHILE @total <= @quantidade
+    BEGIN
+        PRINT 'Tabuada do ' + CAST(@total AS VARCHAR(5));
+        PRINT REPLICATE('-', 13);
+        SET @contador = 0;
+        WHILE @contador <= @limite
+            BEGIN
+                PRINT CAST(@total AS VARCHAR(5)) + ' X ' 
+                    + CAST(@contador AS VARCHAR(5)) + ' = '
+                    + CAST((@total * @contador) AS VARCHAR(5));
+                        SET @contador += 1;
+            END;
+        SET @total += 1;
+        PRINT '';
+    END;
+GO
+
+-----------------------
+-- Stored Procedures --
+-----------------------
+SELECT name
+FROM sys.tables;
+GO
+
+-- Criação de um procedimento --
+CREATE PROCEDURE AloMundo
+AS
+    PRINT 'Alô Mundo!';
+GO
+
+EXECUTE AloMundo;
+GO
+
+-- Informaçoes sobre os procedimentos --
+SELECT * FROM sys.procedures;
+GO
+
+SELECT name      AS 'Procedimentos',
+       create_date AS 'Data de Criação',
+       modify_date AS 'Data de Alteração'
+FROM sys.procedures;
+GO
+
+-- Alterear um procedimento --
+ALTER PROCEDURE AloMundo
+AS
+    PRINT 'Hello World!';
+GO
+
+EXECUTE AloMundo;
+GO
+
+-- Exibe dados do diretorio C:\ --
+EXEC XP_SUBDIRS 'C:\';
+GO
+
+-- Verifica a Existencia de um Arquivo --
+EXECUTE XP_FILEEXIST '';
+
+-- Lista de procedimentos do sistema --
+EXECUTE sp_helpextendedproc;
+GO
+
+
+-- Declaração de variaveis em MySQL --
+DECLARE @dia    AS INT,
+        @mes    AS CHAR(20),
+        @ano    AS INT,
+        @data1  AS DATE,
+        @data2  AS DATETIME;
+
+--Atribuindo Valores --
+SET @dia = DAY(GETDATE());
+SET @mes = MONTH(GETDATE());
+SET @ano = YEAR(GETDATE());
+SET @data1 = DATEFROMPARTS(@ano, @mes, @dia);
+SET @data2 = DATETIMEFROMPARTS(@ano, @mes, @dia, 0, 0, 0, 0);
+
+-- Select --
+-- Do modo que foi feito precisa selecionar tudo para funcionar --
+SELECT @dia    AS 'Dia',
+       @mes    AS 'Mes',
+       @ano    AS 'Ano',
+       @data1  AS 'Data 1',
+       @data2  AS 'Data 2';
+GO
+
+-- Diferença entre datas e horas --
+DECLARE @data1 AS DATE,
+        @data2 AS DATE;
+SET DATEFORMAT DMY;
+SET @data1 = '01/01/2024';
+SET @data2 = GETDATE();
+
+SELECT @data1 AS 'Data Inicial',
+       @data2 AS 'Data Hoje',
+       DATEDIFF(DAY, @data1, @data2)  AS 'Qtd. Dias',
+       DATEDIFF(HOUR, @data1, @data2) AS 'Qtd. Horas';
+GO
+
+-- Modificação de data e valores da hora --
+SELECT GETDATE() AS 'Data atual',
+       DATEADD(MONTH, 5, GETDATE()) AS 'Proximos 5 meses',
+       EOMONTH(GETDATE(), 5)        AS 'Final do mes (5 meses)',
+       SWITCHOFFSET(GETDATE(), '+10:00') AS 'Alteração do fuso (+10 horas)';
+GO
+
+-- Exibe a configuração atual idioma e o primeiro dia da semana --
+SELECT @@LANGUAGE  AS 'Idioma Utilizado',
+       @@DATEFIRST AS 'Primeiro dia da semana';
+GO
+
+-- Demostra a utilização do comando SELECT... CASE --
+SELECT @@LANGUAGE  AS 'Idioma Utilizado',
+    CASE
+        WHEN @@DATEFIRST = 1 THEN 'Segundo-Feira'
+        WHEN @@DATEFIRST = 2 THEN 'Terça-Feira'
+        WHEN @@DATEFIRST = 3 THEN 'Quarta-Feira'
+        WHEN @@DATEFIRST = 4 THEN 'Quinta-Feira'
+        WHEN @@DATEFIRST = 5 THEN 'Sexta-Feira'
+        WHEN @@DATEFIRST = 6 THEN 'Sabado'
+        WHEN @@DATEFIRST = 7 THEN 'Domingo'
+    END AS 'Primeiro Dia da semana';
+GO
+
+-- Exibe os idiomas disponiveis --
+SELECT langid       AS 'Id do idioma',
+       dateformat   AS 'Formato de ddata',
+       datefirst    AS 'Primeiro dia da semana',
+       name         AS 'Nome do idioma',
+       alias        AS 'Nome alternativo',
+       months       AS 'Nome dos meses',
+       shortmonths  AS 'Abreviação dos meses',
+       days         AS 'Nome dos dias'
+FROM sys.syslanguages
+WHERE alias IN ('English','Brazilian','German','Japonese','Russian');
+GO
+
+-- expecificações de um idioma --
+Exec SP_HELPLANGUAGE [Brazilian];
+GO
+
+---------------------------
+-- estilo de data e hora --
+---------------------------
+
+-- Define o idioma a ser utilizado --
+SET LANGUAGE Brazilian;
+GO
+
+-- declara uma variavel para armazenar a data atual --
+DECLARE @data DATETIME;
+
+-- atribui o valor da data atual --
+SET @data = GETDATE();
+
+-- Exibe as informações da data --
+SELECT @@LANGUAGE  AS 'Idioma Utilizado',
+       @data            AS 'Data Atual',
+       DATEPART(DAY,@data)     AS 'Dia do mês',
+       DATENAME(DW, @data)     AS 'Dia da semana',
+       DATEPART(MONTH, @data)  AS 'Mês',
+       DATENAME(MONTH, @data)  AS 'Nome do mês',
+       DATEPART(YEAR, @data)   AS 'Ano',
+       DATENAME(DW,@data)      AS 'Dia da semana',
+       DATENAME(WK, @data)     AS 'Semana do Ano',
+       DATENAME(M, @data)      AS 'Nome do mês',
+       DATENAME(D, @data)      AS 'Dia do mês',
+       DATENAME(DY, @data)     AS 'Dia do ano';
+GO
+
+
+-- verificação de validez de data --
+SET DATEFORMAT DMY;
+GO
+
+IF ISDATE('20/01/2015 00:10:50.000') = 1
+    PRINT 'Data válida!';
+ELSE
+    PRINT 'Data inválida!';
+GO
+
+-- Exemplo de teclados --
+SELECT ASCII('A')     AS 'ASCII: A',
+       UNICODE('A')   AS 'UNICODE: A',
+       CHAR(65)      AS 'CHAR: 65',
+       NCHAR(65)     AS 'NCHAR: 65',
+       ASCII(N'私')    AS 'ASCII: 私',
+       UNICODE(N'私')  AS 'UNICODE: 私',
+       CHAR(31169)   AS 'CHAR: 31169',
+       NCHAR(31169)  AS 'NCHAR: 31169',
+       CHARINDEX('S', 'Microsoft SQL') AS 'CHARINDEX: S',
+       CHARINDEX('SQL', 'Microsoft SQL') AS 'CHARINDEX: SQL';
+GO
+
+-- Uso do SPACE QUOTENAME STR E LEN --
+SELECT 'Paulo' + 'Giovani'             AS 'SPACE 1',
+       'Paulo' + ' ' + 'Giovani'       AS 'SPACE 2',
+       'Paulo' + SPACE(10) + 'Giovani' AS 'SPACE 3',
+       QUOTENAME('Paulo Giovani', '{') AS 'QUOTENAME 1',
+       QUOTENAME('Paulo Giovani', '"') AS 'QUOTENAME 2',
+       QUOTENAME('Paulo Giovani', '[') AS 'QUOTENAME 3',
+       STR(100)                        AS 'STR 1',
+       STR(100.0)                      AS 'STR 2',
+       STR(100.45, 6, 2)               AS 'STR 3',
+       LEN('Paulo Giovani')            AS 'LEM 1';
+GO
+
+-- PATINDEX --
+SELECT PATINDEX('soft', 'Microsoft SQL')   AS 'PATINDEX 1',
+       PATINDEX('%soft%', 'Microsoft SQL') AS 'PATINDEX 2';
+GO
+
+-- SOUNDEX e DIFFERENCE --
+SELECT SOUNDEX('Paulo')            AS 'SOUNDEX: Paulo',
+       SOUNDEX('Paul')             AS 'SOUNDEX: Paul',
+       SOUNDEX('Cris')             AS 'SOUNDEX: Cris',
+       DIFFERENCE('Paulo', 'Paul') AS 'DIFF 1',
+       DIFFERENCE('Paulo', 'Cris') AS 'DIFF 2';
+GO
+
+-- data em diferentes idiomas --
+DECLARE @d DATETIME = GETDATE();
+
+SELECT FORMAT(@d, 'D', 'en-US') AS 'Inglês Americano',
+       FORMAT(@d, 'D', 'en-gb') AS 'Inglês Britânico',
+       FORMAT(@d, 'D', 'de-de') AS 'Alemão',
+       FORMAT(@d, 'D', 'zh-cn') AS 'Chinês Simplificado',
+       FORMAT(@d, 'D', 'pt-br') AS 'Portugês Brasileiro';
+GO
+
+-- CONCAT STUF REVERSE e REPLICATE --
+SELECT CONCAT('Paulo', 'Giovani')                       AS 'CONCAT',
+       CONCAT('Rua','João XXIII, ','15','- São Paulo')  AS 'Endereço',
+       STUFF('Paulo Giovani', 2, 1, 'TEXTO')            AS 'STUFF',
+       REVERSE('Paulo Giovani')                         AS 'REVERSE',
+       REPLICATE('*', 10)                               AS 'REPLICATE 1',
+       'Paulo' + REPLICATE('.', 5) + ':' + '6666-6666'  AS 'REPLICATE 2';
+GO
+
+-- MATH --
+
+SELECT '3.1415'        AS 'PI',
+       PI()            AS 'PI',
+       ABS(-3.1415)    AS 'ABS',
+       CEILING(3.1415) AS 'CEILING',
+       FLOOR(3.1415)   AS 'FLOOR',
+       EXP(1.0)        AS 'EXP',
+       POWER(2,3.0)    AS 'POWER',
+       RAND(5)         AS 'RAND',
+       ROUND(PI(), 2)  AS 'ROUND',
+       SQRT(100)       AS 'SQRT',
+       SIGN(-1)        AS 'SIGN',
+       SQUARE(3)       AS 'SQUARE'
 GO

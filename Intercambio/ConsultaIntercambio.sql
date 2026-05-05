d98974d50291ee3ed933c150d90cf0f2d5381f52
CordeiroOvelha <cordeiro.juttel@gmail.com>
2026-05-04 22:36:31 -0300

    Aula dia 04/05/2026

--- Intercambio/ConsultaIntercambio.sql
+++ Intercambio/ConsultaIntercambio.sql
@@ -532,6 +532,22 @@ GO
 EXEC SP_HELP uspBuscaDadosAlunos;
 GO
 
+-- Informaçoes Mais Especificadas --
+SELECT name        AS 'Nome',
+       create_date AS 'Data de criação',
+       modify_date AS 'Data da ultima modificação'
+FROM sys.procedures
+GO
+
+-- Exemplo 2 --
+SELECT name    AS 'Nome Do Procedimento',
+       definition AS 'definição',
+       type_desc  AS 'Tipo'
+FROM sys.sql_modules M INNER JOIN sys.objects O
+    ON M.object_id = O.object_id
+WHERE type_desc LIKE '%procedure%';
+GO
+
 -- Procedimento volta o maior valor --
 CREATE PROCEDURE uspAchaMaior
     @valor1 FLOAT,
@@ -579,3 +595,127 @@ EXEC uspCOntaIdiomas 'Japonês';
 EXEC uspCOntaIdiomas 'Malgaxe';
 EXEC uspCOntaIdiomas 'Latin';
 GO
+
+--------------------
+-- Aula dia 04/05 --
+--------------------
+
+-- Calcula a idade do alundo --
+CREATE PROCEDURE uspCalculaIdadeAluno
+    @nome AS CHAR(20)
+AS
+    SET NOCOUNT ON;
+
+    DECLARE @data_nascimento DATETIME;
+    DECLARE @idade INT;
+    DECLARE @data_atual DATETIME;
+
+    SET @data_atual = (SELECT GETDATE());
+    SET @data_nascimento = (SELECT DataNasc FROM ALUNOS
+        WHERE NomeAluno = @nome);
+    SET @idade = DATEDIFF(YEAR, @data_nascimento, @data_atual) - CASE WHEN @data_atual <
+        DATEADD(YEAR, DATEDIFF(YEAR, @data_nascimento, @data_atual), @data_nascimento)
+            THEN 1
+            ELSE 0
+    END;
+    SELECT @nome  AS 'Nome do Aluno',
+           @idade AS 'Idade do Aluno';
+    SET NOCOUNT OFF;
+GO
+
+
+EXEC uspCalculaIdadeAluno 'Maria Cristina';
+EXEC uspCalculaIdadeAluno 'Jair Lopes';
+EXEC uspCalculaIdadeAluno 'Miguel Firmino';
+
+
+-- Valor Medio das viagens --
+CREATE PROCEDURE uspMediaViagens
+    @pais AS VARCHAR(50),
+    @media MONEY OUTPUT
+AS
+    SET @media = (SELECT AVG(Valor) AS 'Media dos Valores'
+        FROM VIAGENS
+        WHERE PaisDestino = (SELECT CodPais FROM PAISES
+            WHERE NomePais = @pais));
+GO
+
+DECLARE @pais VARCHAR(50) = 'Estados Unidos da América';
+DECLARE @resultado MONEY;
+
+EXEC uspMediaViagens @pais, @resultado OUTPUT;
+PRINT 'Custo Médio das viagens realizadas para o ' + @pais + ': R$ ' + CAST(@resultado AS VARCHAR);
+GO
+
+-- Exclui o stored procedure --
+DROP PROCEDURE uspSaudacao;
+GO
+
+-- Não Existe mais
+EXEC uspSaudacao 'Paulo';
+GO
+
+-------------
+-- Funções --
+-------------
+CREATE FUNCTION fatorial (@N INT)
+    RETURNS BIGINT
+AS
+    BEGIN
+        DECLARE @fat BIGINT,
+                @i   INT;
+        SET @fat = 1;
+        SET @i = 1;
+        IF (@N <= 1)
+            RETURN @fat;
+        ELSE
+            WHILE (@i < @N)
+                BEGIN
+                    SET @fat = @fat * @i;
+                    SET @i = @i + 1;
+                END
+            RETURN @fat;
+    END;
+GO
+
+SELECT dbo.fatorial(20) AS 'Fatorial de 20',
+       dbo.fatorial(5)  AS 'Fatorial de 5';
+GO
+
+PRINT dbo.fatorial(10);
+GO
+
+-- Todos os dados das viagens dos alunos --
+SELECT A.CodAluno    AS 'Codigo Aluno',
+       A.NomeAluno   AS 'Nome Aluno',
+       A.Genero      AS 'Genero Aluno',
+       A.Endereco    AS 'Endereço Aluno',
+       A.PaisOrigem  AS 'Pais de Origem Aluno',
+       V.PaisDestino AS 'Pais de Destino Aluno',
+       V.Valor       AS 'Valor da viagem'
+FROM ALUNOS A INNER JOIN VIAGENS V
+    ON A.CodAluno = V.CodViagem;
+GO
+
+-- Função de Alinhamento --
+CREATE FUNCTION AlinhaCampo(@texto VARCHAR(255), @tamanho TINYINT, @caractere CHAR(1))
+    RETURNS VARCHAR(255)
+AS
+    BEGIN
+        DECLARE @resultado VARCHAR(255);
+        SET @resultado = REPLICATE(@caractere, @tamanho - LEN(@texto)) + @texto;
+        RETURN @resultado;
+    END;
+GO
+
+SELECT dbo.AlinhaCampo(A.CodAluno, 2, 0)                 AS 'Codigo Aluno',
+       A.NomeAluno   AS 'Nome Aluno',
+       A.Genero      AS 'Genero Aluno',
+       A.Endereco    AS 'Endereço Aluno',
+       A.PaisOrigem  AS 'Pais de Origem Aluno',
+       V.PaisDestino AS 'Pais de Destino Aluno',
+       dbo.AlinhaCampo(CAST(V.Valor AS VARCHAR), 10, 0)  AS 'Valor da viagem'
+FROM ALUNOS A INNER JOIN VIAGENS V
+    ON A.CodAluno = V.CodViagem;
+GO
+

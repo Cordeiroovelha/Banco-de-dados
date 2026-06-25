8e2cbb0b210e4a9fb27920e1a66bbdaf193795ce
Murilo Jutte Cordeiro <cordeiro.juttel@gmail.com>
2026-06-25 15:54:40 -0300

    projeto final part 2

---
+++ MongoDB/Coleçoes.sql
@@ -0,0 +1,19 @@
+{
+  "_id": 1,
+  "nome": "Jose da Silva",
+  "idade": 45,
+  "contatoEmergencia": "(11)98765-4321",
+  "tipoSanguineo": "O-",
+  "prioridade": 2,
+  "descricao": "Dor de cabeça",
+  "historicoConsultas": [
+    {
+      "consultaId": 3,
+      "medicoId": 5,
+      "data": "2024-11-21",
+      "situacao": "Em atendimento",
+      "ala": "Emergencia"
+    }
+    // ... outras consultas do paciente
+  ]
+}
